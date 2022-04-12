b95fb3a6538d2aa3f8fa3242fdb44ff2a4094678
Bami85 <91563447+Bami85@users.noreply.github.com>
2022-04-12 09:41:01 +0200

    Update footer.php

---
+++ wp-content/themes/webshopTheme/footer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Cosmetics products</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="footerstyle.css" rel="stylesheet">
+</head>
+<body>
+
+
+<header>
+    <p>
+   Våra body </p>
+</header>
+    
+<footer class="footer">
+    <div class="l-footer">
+    <h1>
+    <img src="" alt="">Här Logo</h1>
+    <p>
+    Vi kan skriva en slogan hr om ni vill </p>
+    </div>
+    <ul class="r-footer">
+    <li>
+      <h2>
+    CONTACT</h2>
+    <ul class="box">
+    <li><a href="#">Facebook</a></li>
+    <li><a href="#">Twitter</a></li>
+    <li><a href="#">Pinterest</a></li>
+    <li><a href="#">Dribbble</a></li>
+    </ul>
+    </li>
+    <li class="features">
+      <h2>
+    INFORMATION</h2>
+    <ul class="box h-box">
+    <li><a href="#">About us </a></li>
+    </ul>
+    </li>
+    <li>
+      <h2>
+    SUBSCRIBE TO OUR NEWSLETTER</h2>
+    <ul class="box">
+    <li><a href="#">SKAPA ETT FORM FORMULÄR</a></li>
+    </ul>
+    </li>
+    </ul>
+    <div class="b-footer">
+    <p>
+    All rights reserved by ©BioProducter 2022 </p>
+    </div>
+ </footer>
