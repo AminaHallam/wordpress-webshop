bdfdb3dd2539a01aa2f89b2499d05f4e27fad9f8
Amïna <88747914+AminaHallam@users.noreply.github.com>
2022-04-12 15:23:08 +0200

    created-homePage-and-contactPage

--- wp-content/themes/webshopTheme/page-om-oss.php
+++ wp-content/themes/webshopTheme/page-om-oss.php
@@ -2,39 +2,55 @@
 get_header(); ?> 
 
 <main> 
+    <aside id="secondary" class="col-xs-12 col-md-3">
+                                <div id="sidebar">
 
-<?php while(have_posts()) {
-    the_post(); ?> 
+                                    <ul role="navigation">
+                                        <li class="pagenav">
+
+                                            <?php 
+                                            /* Hämtar lämplig widget  */
+                                            dynamic_sidebar('widget-sidmenu');
+                                            ?>
+                                        </li>
+                                    </ul>
+
+                                </div>
+    </aside>
+
 
     <section>
+        <?php while(have_posts()) {
+            the_post(); ?> 
 
-        <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
+            <div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
 
-                        <h1>
-                            <?php the_title(); ?> 
-                        </h1> 
+                            <h1>
+                                <?php the_title(); ?> 
+                            </h1> 
 
-                        <?php the_post_thumbnail(); ?> 
+                            <?php the_post_thumbnail(); ?> 
 
-                        <div class="text">
-                            
-                            <p><?php the_content(); ?></p>
+                            <div class="text">
+                                
+                                <p><?php the_content(); ?></p>
+
+                            </div>
 
                         </div>
 
                     </div>
-
                 </div>
             </div>
-        </div>
 
+            
+        <?php } ?>
     </section>
-
-    <?php } ?>
 
 </main> 
 
 <?php get_footer(); ?> 
+
