3d9881116bfd34d674f7888417de0a310da6d6e1
Amïna <88747914+AminaHallam@users.noreply.github.com>
2022-05-02 10:33:57 +0200

    worked-with-categories-and-Css-in-startPage

--- wp-content/themes/webshopTheme/enqueue.php
+++ wp-content/themes/webshopTheme/enqueue.php
@@ -8,15 +8,13 @@ function my_scripts_css() {
     wp_register_style('style', get_template_directory_uri().'/style.css'); 
     /* wp_register_style('fontawesome', get_template_directory_uri().'/css/font-awesome.css');  */
     
-    wp_register_script('jQuery', get_template_directory_uri().'/js/jquery.js', [], false, true); 
     wp_register_script('bootstrap-js', get_template_directory_uri().'./js/bootstrap.min.js', [], false, true);
-    /* wp_register_script('bootstrap', get_template_directory_uri().'./js/bootstrap.js', [], false, true); */
 
     wp_enqueue_style('bootstrap');
     wp_enqueue_style('style');
     /* wp_enqueue_style('font-awesome'); */
 
-    wp_enqueue_script('jquery');
+    
     wp_enqueue_script('bootstrap-js');
 
 
