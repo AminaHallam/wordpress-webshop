c946f4ebb23eb85f33a16e4a7c075a6c313d5a8e
Hannane <hannanekabuli@gmail.com>
2022-05-02 14:01:45 +0100

    footer-css

--- wp-content/themes/webshopTheme/footer.php
+++ wp-content/themes/webshopTheme/footer.php
@@ -19,7 +19,7 @@
 							?>
 					</div>
 
-                    <div class="col-xs-12 col-sm-3 col-md-3 col-md-offset-1">
+                    <div class="col-xs-12 col-sm-6 col-md-3 col-md-offset-1">
 					<ul class="social">
 						<li>
                         <?php 
