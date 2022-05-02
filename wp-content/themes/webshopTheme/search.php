fe9aed0336bb807e2e37b784293258d93d7d681d
Amïna <88747914+AminaHallam@users.noreply.github.com>
2022-05-02 12:52:59 +0200

    created-search-input-and-edited-search-page

--- wp-content/themes/webshopTheme/search.php
+++ wp-content/themes/webshopTheme/search.php
@@ -14,20 +14,37 @@ get_header(); ?>
                     <div class="col-xs-12">
 
                         <h1>
-                            <?php the_title(); ?> 
-                        </h1> 
+                            <a id="titlePost" href="
+                            <?php  
+                            
+                            the_permalink_rss(); 
+                            ?>">
+                            <?php 
+                            
+                            the_title(); 
+                            
+                            ?> 
+                            </a>
 
-                        <?php the_post_thumbnail(); ?> 
+                        </h1> 
+                        <div class="searchDiv">
+                        <img class="searchImg"  src="
+								<?php 
+								the_post_thumbnail_url(); 
+								?>">
+
+                        </div>
 
                         <div class="text">
                             
-                            <p><?php the_content(); ?></p>
+                            <p><?php the_excerpt(); ?></p>
 
                         </div>
 
                     </div>
 
                 </div>
+                <hr style="width:70%;margin: auto;"> 
             </div>
         </div>
 
