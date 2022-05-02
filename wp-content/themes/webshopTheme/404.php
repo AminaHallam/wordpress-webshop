01f0e3505ac5f1d1cdcad680deb703fa242dfbd1
Amïna <88747914+AminaHallam@users.noreply.github.com>
2022-05-02 13:39:00 +0200

    added-404Page

---
+++ wp-content/themes/webshopTheme/404.php
@@ -0,0 +1,17 @@
+<?php
+get_header(); ?> 
+
+<main> 
+    <div class="container">
+        <p class="errorPage">
+        <?php 
+
+        echo 'Nothing was found at this location. Try searching, or check out the links below.'; 
+
+        ?> 
+
+        </p>
+    </div>
+</main> 
+
+<?php get_footer(); ?> 
